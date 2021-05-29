46a173c0803b40723e3b3df56927c3bd3061c975
Jan <55839828+Piitschy@users.noreply.github.com>
2021-05-29 12:19:29 +0200

    implement clearing

--- bot.py
+++ bot.py
@@ -5,14 +5,21 @@ import random
 import re
 import platform
 from playsound import playsound
+import os
 
+clear_str = {
+  'Linux': 'clear',
+  'Darwin': 'clear',
+  'Windows': 'cls',
+}
 
-os = platform.system()
+system = platform.system()
 
 rxDate=r'^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$'
 rxPLZ=r'^[0-9]{5}$'
 
 rand = lambda: 5+random.randint(1, 10)/5
+clear = lambda: os.system(clear_str[system])
 
 def eingabe(text, regex):
   e = input(text)
@@ -21,10 +28,11 @@ def eingabe(text, regex):
   print('Ne, mach mal richtig...')
   eingabe(text, regex)
 
+clear()
 geb=eingabe('Geburtstag (dd.MM.yyyy): ',rxDate)
 plz=eingabe('PLZ: ',rxPLZ)
 
-session = ImpfBot(os)
+session = ImpfBot(system)
 session.anmeldung(geb,plz)
 session.refresh()
 
